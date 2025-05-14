e11c0bee149ff89629147d50746542ad4288a8b7
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-05-13 22:21:14 -0300

    feat: configurando as variáveis de ambiente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
+import { envSchema } from '@/infra/env/env';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+  ],
 })
 export class AppModule {}
