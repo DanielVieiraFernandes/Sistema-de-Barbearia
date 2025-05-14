797e9a6cd741f69d8a193674ccd104ef8424cd7b
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-05-14 16:43:05 -0300

    feat: alterando configurações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from '@/infra/auth/auth.module';
 import { envSchema } from '@/infra/env/env';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
@@ -8,6 +9,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       validate: (env) => envSchema.parse(env),
     }),
+    AuthModule,
   ],
 })
 export class AppModule {}
